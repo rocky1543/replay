e4a4d24ea729f24896cf324013d8f5f257826255
alalei1543 <18710179311@163.com>
2024-01-21 13:29:29 +0800

    fix code

--- download_data.py
+++ download_data.py
@@ -1,34 +1,30 @@
-import time
-
-import akshare as ak
 import tushare as ts
 
 
-def download_data():
+def download_data(start_day, end_day):
     # 设置Tushare接口的token，你需要先在Tushare官网注册并获取token
     ts.set_token('087c527f2572b83dd1a7ce2e101c8a45c56101df1652b3ca928bfe3b')
 
     # 初始化Tushare接口
     pro = ts.pro_api()
 
-    day_list = get_day_list()
-    for date in day_list[:1]:
+    day_list = get_day_list(start_day, end_day)
+    for date in day_list:
+        print("download_data_day:", date)
         # 获取指定日期的A股收盘数据
         df = pro.daily(trade_date=date)
 
-        # 输出获取到的收盘数据
-        print(df)
         # 将DataFrame保存为CSV文件
         df.to_csv('./close_data/收盘数据-{}.csv'.format(date))
 
 
-def get_day_list():
+def get_day_list(start_day, end_day):
     from datetime import datetime
     from dateutil.relativedelta import relativedelta
 
     # 起始日期和结束日期
-    start_date = datetime.strptime('20230828', '%Y%m%d')
-    end_date = datetime.strptime('20240102', '%Y%m%d')
+    start_date = datetime.strptime(start_day, '%Y%m%d')
+    end_date = datetime.strptime(end_day, '%Y%m%d')
 
     # 获取日期列表
     date_list = [start_date + relativedelta(days=i) for i in range(0, (end_date - start_date).days + 1)]
@@ -41,4 +37,6 @@ def get_day_list():
 
 
 if __name__ == '__main__':
-    download_data()
+    start_day = "20230828"
+    end_day = "20240102"
+    download_data(start_day, end_day)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import time
+
 import akshare as ak
+import tushare as ts
 
 
 def test1():
@@ -16,5 +19,56 @@ def test2():
     print(data_arr)
 
 
+def test3():
+    df = ak.stock_zh_a_spot_em()
+    # ak.stock_zh_a_close()
+
+    code_map = {}
+    code_list = []
+    for index, row in df.iterrows():
+        code_map[row["名称"].strip()] = {"代码": row["代码"].strip(), "最高": row["最高"]}
+        code_list.append(row["代码"].strip())
+    print("code_map:", code_map)
+    print("code_list:", code_list)
+    print("code_list_len:", len(code_list))
+
+    sz_high_price_day = ["20230828", "20231121", "20231229"]
+
+    start_time = int(time.time() * 1000)
+    for i, code in enumerate(code_list[:30]):
+        print("code:", code)
+        if i % 100 == 0:
+            print("count:", i)
+            print("use_time:", (int(time.time() * 1000) - start_time))
+
+        hist_df = ak.stock_zh_a_hist(symbol="002176", start_date='20280828', end_date='20240121')
+        high_price_list = []
+        sz_hp_day_price_list = []
+        print("hist_df:", hist_df)
+        for index, row in hist_df.iterrows():
+            print("index:", index)
+            print("row:", row)
+            high_price_list.append(row["最高"])
+
+        print("high_price_list:", high_price_list)
+
+
+def test4():
+    # 设置Tushare接口的token，你需要先在Tushare官网注册并获取token
+    ts.set_token('087c527f2572b83dd1a7ce2e101c8a45c56101df1652b3ca928bfe3b')
+
+    # 初始化Tushare接口
+    pro = ts.pro_api()
+
+    # 获取指定日期的A股收盘数据
+    date = '20230620'  # 指定日期
+    df = pro.daily(trade_date=date)
+
+    # 输出获取到的收盘数据
+    print(df)
+    # 将DataFrame保存为CSV文件
+    df.to_csv('output.csv')
+
+
 if __name__ == '__main__':
-    test2()
+    test4()
