23cfe5fcd2494fc83f6ab947faae172292e7e3c3
alalei1543 <18710179311@163.com>
2024-03-12 21:05:07 +0800

    add code

--- zhangting_spider.py
+++ zhangting_spider.py
@@ -157,10 +157,10 @@ def save_word_text(ti_cai, info_map, lao_long_gao_du, cycle_and_action, print_ty
         zhu1 = "节点: {}， 老龙高度: {}板".format(cycle_and_action.get("cycle"), lao_long_gao_du)
         zhu2 = "空间: {}".format(cycle_and_action.get("profit_space"))
         zhu3 = "计划: {}".format(cycle_and_action.get("action"))
-        zhu4 = "注: 你越是急着找一样东西，越是偏偏找不到，聪明的话根本不用找，他会慢慢自己出来；" \
-               "时机、属性、空间、身位、实力；自上而下，看高做低；pk淘汰赛在哪里，会不会继续，谁是第一名"
+        zhu4 = "注1: 大把机会，你越是急着找一样东西，越是偏偏找不到，聪明的话根本不用找，他会慢慢自己出来"
+        zhu5 = "注2: 时机、属性、空间、身位、实力；自上而下，看高做低；pk淘汰赛在哪里，会不会继续，谁是第一名"
 
-        zhu = zhu1 + "\n" + zhu2 + "\n" + zhu3 + "\n" + zhu4
+        zhu = zhu1 + "\n" + zhu2 + "\n" + zhu3 + "\n" + zhu4 + "\n" + zhu5
         # 添加段落
         doc.add_paragraph(title + "\n" + info + "\n\n" + ti_cai_text + "\n\n" + zhu)
 
