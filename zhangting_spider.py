91280a0c9db7b6c8675768a54b058e3e10e71a43
alalei1543 <18710179311@163.com>
2024-06-04 22:03:52 +0800

    add code

--- zhangting_spider.py
+++ zhangting_spider.py
@@ -164,7 +164,7 @@ def save_word_text(ti_cai, info_map, lao_long_gao_du, cycle_and_action, print_ty
         # zhu2 = "空间: {}".format(cycle_and_action.get("profit_space"))
         # zhu3 = "计划: {}".format(cycle_and_action.get("action"))
         zhu4 = "亏钱永远比赚钱容易，因为市场不好的时候，亏得最惨的一般都是追高的，" \
-               "市场好的时候，涨起来的是因为新闻助推的\n" \
+               "市场好的时候，涨起来的是因为新闻助推的，方向是随机，你不一定跟得上\n" \
                "越是急着，越是找不到，当个傻子吧，不是风动，不是幡动，是心在动"
         zhu5 = "任何一场战争都是以强胜弱，以多胜少，狮子打兔子，牛刀杀鸡，没有这个条件只能等\n" \
                "虚灵顶劲，复盘核心：自上而下：1、先研究主流题材；2、再研究主流题材人气核心标的"
