93a09a289949c92c09d7499492341384473e581a
alalei1543 <18710179311@163.com>
2025-11-17 22:18:59 +0800

    fix code

--- zhangting_spider.py
+++ zhangting_spider.py
@@ -467,7 +467,7 @@ def get_name_list(file, filter_list):
 
 
 def get_replay_name_list():
-    bu_zhang_long = get_name_list("input/1-补涨龙.txt", [])
+    bu_zhang_long = get_name_list("input/1-龙头属性.txt", [])
     lian_ban = get_name_list("input/2-连板情绪.txt", bu_zhang_long)
     filter_list = bu_zhang_long + lian_ban
     fu_pan = get_name_list("input/3-大盘核心.txt", filter_list)
