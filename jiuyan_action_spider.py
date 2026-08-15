2ca9b7bdd99b3d734436abafb53840b0ef1caeea
luofu <luofu@blued.com>
2026-08-15 14:36:51 +0800

    add code

--- jiuyan_action_spider.py
+++ jiuyan_action_spider.py
@@ -38,7 +38,7 @@ import argparse, csv, json, os, socket, subprocess, sys, time, urllib.request
 from playwright.sync_api import sync_playwright
 
 URL = 'https://www.jiuyangongshe.com/action'
-STATE_FILE = '~/Downloads/tmp/jiuyan_state.json'
+STATE_FILE = '/Users/xiaoguaiguai/Downloads/tmp/jiuyan_state.json'
 
 UA = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
